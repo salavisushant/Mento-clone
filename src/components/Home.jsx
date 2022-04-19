42dfac5d0cd346a3d5d5bddeebd5ebfa22467b8d
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-04-19 15:31:44 +0530

    data fetched

---
+++ src/components/Home.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import {Link} from "react-router-dom"
+
+
+import {
+    getTodoError,
+    getTodoLoading,
+    getTodoSuccess,
+} from "../redux/Action";
+
+
+export const Home = () => {
+
+    const dispatch = useDispatch();
+    
+    const { todos} = useSelector((state) => ({
+      todos: state.todos,
+    }));
+
+    useEffect(() => {
+        showData()
+    },[])
+
+
+    const showData = async() => {
+        try{
+            dispatch(getTodoLoading());
+            const data = await fetch(`  http://localhost:3001/posts`)
+            .then((d) => d.json());
+            dispatch(getTodoSuccess(data));
+            console.log(data);
+        } catch (err) {
+            dispatch(getTodoError(err));
+      }
+    }
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
