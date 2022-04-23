f2e75fcada21d4a7d981ebea2b97b029955fe9a9
salavisushant <87421852+salavisushant@users.noreply.github.com>
2022-04-23 11:36:00 +0530

    fixed the bug of filter function

--- src/components/Topic.jsx
+++ src/components/Topic.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom"
 import "./topic.css";
-
+import {Navbar} from "./Navbar"
 
 
 
@@ -10,6 +10,7 @@ import "./topic.css";
 
 export const Topic = () => {
     const [list, setList] = useState([]);
+    const [data,setData] = useState([])
   
 
     useEffect(() => {
@@ -28,67 +29,61 @@ export const Topic = () => {
         const rows = [...list].filter((data) => {
             return data.topic === "entrepreneurship";
         });
-        setList(rows)
+        setData(rows)
+        console.log(rows);
     }
 
      const handleCheckFilm = () => {
-        const rows = [...list].filter((row) => row.topic === "film making");
-        setList(rows) 
+         const rows = [...list].filter((row) => row.topic === "film making");
+
+        setData(rows) 
     };
     
      const handleCheckIllustrar = () => {
         const rows = [...list].filter((row) => row.topic === "illustrator");
-        setList(rows) 
+        setData(rows) 
     };
 
      const handleCheckFashion = () => {
         const rows = [...list].filter((row) => row.topic === "fashion blogging");
-        setList(rows) 
+        setData(rows) 
     };
 
      const handleCheckComedian = () => {
         const rows = [...list].filter((row) => row.topic === "comedian");
-        setList(rows) 
+        setData(rows) 
     };
 
      const handleCheckPhotograph = () => {
-        const rows = [...list].filter((row) => row.topic === "Photographer");
-        setList(rows) 
+         const rows = [...list].filter((row) => row.topic === "Photographer");
+         console.log(rows);
+        setData(rows) 
     };
 
     const handleCheckRadio = () => {
         const rows = [...list].filter((row) => row.topic === "radio jockey" );
-        setList(rows) 
+        setData(rows) 
     };
 
      const handleCheckBitBoxing = () => {
-        const rows = [...list].filter((row) => row.topic === "beat boxing");
-        setList(rows) 
+         const rows = [...list].filter((row) => row.topic === "beat boxing");
+        setData(rows) 
     };
+
+    const handleAllData = () => {
+        setData([])
+    }
 
   
 
     return (
         <div>
-             <div style={{display: 'flex',marginTop:'1%'}}>
-                <div>
-                    <Link to="/"><img style={{height:'50px',marginLeft:'10px'}} src="https://www.mento.co.in/assets/logo-e65920660caecc5be1d6b1757278bcb5745b83cfbf08d0dcdc5cd30bead06334.svg" alt=""/></Link>
-                </div>
-                <div className="rightNavDiv">
-                     <form>
-                        <input type="search" placeholder="Topic name,Influences name"/>
-                        <button type="submit">Search</button>
-                    </form>
-                        <button className="btn1">ALL TOPICS</button>
-                        <button className="btn1">ABOUT</button>
-                        <button className="btn2">LOGIN</button>
-                </div>
-            </div>
+           <Navbar/>
             <p className="lessTag">ALL LESSONS</p>
             <div className="topicDiv">
                 <div>
-                    <p className="filterName">ALL TOPICS</p>
-                    <p className="filterName" onClick={handleCheckFilm }>FILM-MAKING</p>
+                    <p className="filterName"  onClick={handleAllData}>ALL TOPICS</p>
+                    <p className="filterName"  onClick={handleCheckFilm }>FILM-MAKING</p>
                     <p className="filterName"  onClick={handleCheckBitBoxing}>BEAT BOXING</p>
                     <p className="filterName"  onClick={handleCheckEntrepreneur}>ENTREPRENEURSHIP</p>
                     <p className="filterName"  onClick={handleCheckFashion}>FASHION BLOGGING</p>
@@ -98,9 +93,28 @@ export const Topic = () => {
                     <p className="filterName"  onClick={handleCheckPhotograph}>PHOTOGRAPHY</p>
                 </div>
                 <div className="gridDiv">
-                {
-                    list.map((e) => (
+                    {
+                    data.length?
+                    data.map((e) => (
                         <Link style={{textDecoration: 'none',color: '#000000'}} to={`/posts/${e.id}`}>
+                             <div className="infoDiv">
+                            <img className="imgDiv" src={e.image} alt="" />
+                            <p className="title1">{e.title}</p>
+                            <div style={{display:'flex'}}>
+                                <img className='roundImg' src={e.image} alt="" />
+                                <div className="nameDiv">
+                                    <p className="authText">{e.author}</p>
+                                    <p className="company">{ e.comapany}</p>
+                                </div>
+                                <div className="videoDIV">
+                                    <p className="authText" style={{marginLeft:'20%'}}>{e.video}</p>
+                                    <p className="company">VIDEOS</p>
+                                </div>
+                            </div>
+                        </div>
+                        </Link>
+                    )) : list.map((e) => (
+                         <Link style={{textDecoration: 'none',color: '#000000'}} to={`/posts/${e.id}`}>
                              <div className="infoDiv">
                             <img className="imgDiv" src={e.image} alt="" />
                             <p className="title1">{e.title}</p>
